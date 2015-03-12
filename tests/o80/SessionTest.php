12df3c02dbdf9da27eab1049bf8b34373bf905af
Olivier PEREZ <olivier@olivierperez.fr>
2015-03-13 00:32:30 +0100

    Complete unit tests on IP detection

--- tests/o80/SessionTest.php
+++ tests/o80/SessionTest.php
@@ -3,10 +3,24 @@ namespace o80;
 
 class SessionTest extends \PHPUnit_Framework_TestCase {
 
-    public function testFindDirectIp() {
-        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+    /**
+     * @dataProvider ipsProvider
+     */
+    public function testShouldFindIpOfClient($remoteAddr, $forwardedFor, $expected) {
+        $_SERVER['REMOTE_ADDR'] = $remoteAddr;
+        $_SERVER['HTTP_X_FORWARDED_FOR'] = $forwardedFor;
         $ip = Session::ip();
-        $this->assertEquals('127.0.0.1', $ip);
+        $this->assertEquals($expected, $ip);
+    }
+
+    public function ipsProvider() {
+        return array(
+            array('RA', null, 'RA'),
+            array('RA', 'FF', 'FF'),
+            array(null, 'FF', 'FF'),
+            array('RA', 'FF1,FF2', 'FF1'),
+            array(null, 'FF1,FF2', 'FF1')
+        );
     }
 
 }

b177319a965421cf67a8d9995da61ea6525bfc67
Olivier PEREZ <olivier@olivierperez.fr>
2015-03-12 18:50:49 +0100

    Add sources + README.MD + composer.json

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # o80-session
+
+This PHP library provide some security around the stealing of session.
+
+# How-to
+
+## Starting session
+
+Replace `session_start();` by `o80\Session::start();`.
+
+## Using session
+
+Nothing changed :
+
+```php
+// Writing
+$_SESSION['x'] = 'foo';
+
+// Reading
+$bar = $_SESSION['x'];
+```
+
+# Contribution
+
+Just fork the project, make your changes, ask for pull request ;-).
---
+++ src/o80/Session.php
@@ -0,0 +1,95 @@
+<?php
+namespace o80;
+
+class Session {
+
+    const SESSION_MASTER_KEY = 'o80-session';
+
+    /**
+     * This method secures the call to session_start() fonction.
+     */
+    public static function start() {
+        session_start();
+
+        if (self::get('IP') === null) {
+            // First creation
+            self::newSession();
+        } else if ( // Check if stored values are still the same
+            self::get('AGENT') !== self::fromServer('HTTP_USER_AGENT')
+            || self::get('LANGUAGE') !== self::fromServer('HTTP_ACCEPT_LANGUAGE')
+            || self::get('CHARSET') !== self::fromServer('HTTP_ACCEPT_CHARSET')
+            || self::get('ENCODING') !== self::fromServer('HTTP_ACCEPT_ENCODING')
+            || self::get('IP') !== self::ip()
+        ) {
+            // If one of the stored valed changed, we create a new session
+            self::newSession();
+        }
+    }
+
+    /**
+     * This method get the user IP address.
+     *
+     * @return string the IP address
+     */
+    private static function ip() {
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            if (strchr($_SERVER['HTTP_X_FORWARDED_FOR'], ',')) {
+                $ips = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+                $realip = $ips[0];
+            } else {
+                $realip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            }
+        } else {
+            $realip = $_SERVER['REMOTE_ADDR'];
+        }
+
+        return $realip;
+    }
+
+    /**
+     * Generate a new session. This method is call when the session is created or when an attack is detected.
+     */
+    private static function newSession() {
+        // New session
+        session_regenerate_id();
+        $_SESSION = array();
+
+        // Store new values
+        self::put('AGENT', self::fromServer('HTTP_USER_AGENT'));
+        self::put('LANGUAGE', self::fromServer('HTTP_ACCEPT_LANGUAGE'));
+        self::put('CHARSET', self::fromServer('HTTP_ACCEPT_CHARSET'));
+        self::put('ENCODING', self::fromServer('HTTP_ACCEPT_ENCODING'));
+        self::put('IP', self::ip());
+    }
+
+    /**
+     * Store a value in session.
+     *
+     * @param $key string the key of the value
+     * @param $value mixed the value to store
+     */
+    private static function put($key, $value) {
+        $_SESSION[self::SESSION_MASTER_KEY][$key] = $value;
+    }
+
+    /**
+     * Read a value from session.
+     *
+     * @param $key string the key of the value
+     * @return mixed|null null is the value is not found
+     */
+    private static function get($key) {
+        return isset($_SESSION[self::SESSION_MASTER_KEY][$key]) ? $_SESSION[self::SESSION_MASTER_KEY][$key] : null;
+    }
+
+    /**
+     * Get a value from $_SERVER.
+     *
+     * @param $key string the key of the value
+     * @return string|null null is the value is not found
+     */
+    private static function fromServer($key) {
+        return !empty($_SERVER[$key]) ? $_SERVER[$key] : '';
+    }
+
+}
